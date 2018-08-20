c420f188ab29a2394c92a53c077b0497f31b1449
Salma Mohammed <sm.71175@gmail.com>
2018-08-20 19:26:48 +0200

    fix a bug.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller('HomeController', ['$scope', 'shows', function($scope, shows) {
+app.controller('MainController', ['$scope', 'shows', function($scope, shows) {
   shows.success(function(data) {
     $scope.shows = data;
   });
